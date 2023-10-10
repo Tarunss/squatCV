d173ecb9f03fee688ed810f0efe4c84fc1fd72d3
tsohal1 <tsohal@ucsc.edu>
2023-10-10 14:25:00 -0700

    Added code to analyze pose

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture()
+
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv2.VideoCapture('PoseVideos/squat_front_view.mp4')
+pTime = 0
+while True:
+    success, img = cap.read()
+    #Capture runs on different color, so we need to convert
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    if (results.pose_landmarks):
+        mpDraw.draw_landmarks(img,results.pose_landmarks,mpPose.POSE_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img,str(int(fps)),(70,50),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+    cv2.imshow("Image",img)
+    cv2.waitKey(10)
+    
 #This is a comment to check if github is working correctly
 
